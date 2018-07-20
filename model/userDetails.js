d2683a253c8bfbdfa129f914b5c0d5ba6db92124
lewmoore <lewis.moore2707@gmail.com>
2018-07-20 13:24:30 +0100

    Edit model to accept jkust name

--- model/userDetails.js
+++ model/userDetails.js
@@ -2,8 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userDetailsSchema = new mongoose.Schema({
-  firstname: String,
-  lastname: String,
+  name: String,
   email: { type: String, unique: true }
 })
 
